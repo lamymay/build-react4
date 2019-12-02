eaf12d0ebbf7dbf02b1d2850a2ad92ce87745899
lamymay <lamymay@outlook.com>
2019-12-02 19:41:40 +0800

    v12-02-19:41

---
+++ precache-manifest.32c6075521afc757f915e5afe3127435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40b7dff334d5111dd3e1f80a18a71ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e49fca06454682c54993",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "0eefc049bd1b2f8c9935",
+    "url": "/static/css/main.db7f4f88.chunk.css"
+  },
+  {
+    "revision": "e49fca06454682c54993",
+    "url": "/static/js/2.c8c55177.chunk.js"
+  },
+  {
+    "revision": "0eefc049bd1b2f8c9935",
+    "url": "/static/js/main.71fc27d4.chunk.js"
+  },
+  {
+    "revision": "0bf5d7a0750782d31d71",
+    "url": "/static/js/runtime~main.da90fee4.js"
+  },
+  {
+    "revision": "758a4c9ebe8a8035e4a5298a302e5390",
+    "url": "/static/media/avatar.758a4c9e.jpeg"
+  }
+]);
